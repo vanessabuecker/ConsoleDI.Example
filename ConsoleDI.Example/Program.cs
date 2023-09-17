27cba25472dd3ad9a43ef9f0ced4f08892d93bc4
VanessaBuecker <v.buecker@rocketmail.com>
2023-09-17 09:43:13 -0300

    Registrando serviços para DI

--- ConsoleDI.Example/Program.cs
+++ ConsoleDI.Example/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ConsoleDI.Example;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddTransient<IExampleTransientService, ExampleTransientService>();
+builder.Services.AddScoped<IExampleScopedService, ExampleScopedService>();
+builder.Services.AddSingleton<IExampleSingletonService, ExampleSingletonService>();
+builder.Services.AddTransient<ServiceLifetimeReporter>();
+
+using IHost host = builder.Build();
+
+ExemplifyServiceLifetime(host.Services, "Lifetime 1");
+
+
+static void ExemplifyServiceLifetime(IServiceProvider hostprovider, string lifetime)
+{
+    using IServiceScope serviceScope = hostprovider.CreateScope();
+    IServiceProvider provider = serviceScope.ServiceProvider;
+    ServiceLifetimeReporter logger = provider.GetRequiredService<ServiceLifetimeReporter>();
+        logger.ReportServiceLifetimeDatails(
+            $"{lifetime}: Call 1 to provider.GetRequiredService<ServiceLifetimeReporter>()");
+
+    Console.WriteLine("...");
+
+    logger = provider.GetRequiredService<ServiceLifetimeReporter>();
+    logger.ReportServiceLifetimeDatails(
+        $"{lifetime}: Call 2 to provider.GetRequiredService<ServiceLifetimeReporter>()");
+    Console.WriteLine();
+}
